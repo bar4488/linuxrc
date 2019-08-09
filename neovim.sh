90bb2eb504a0e5c99aa1e83f267ea3f396857c2a
Bar Tzadok <bar4488@gmail.com>
2019-08-09 03:50:20 +0300

    added installation script for various programs

---
+++ neovim.sh
@@ -0,0 +1,3 @@
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
---
+++ nodejs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+sudo apt -y install gcc g++ make
+sudo apt -y install nodejs
